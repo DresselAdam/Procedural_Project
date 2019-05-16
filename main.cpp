9410789d8682aeeca28a7febbcb584ccbc20e77e
DresselAdam <amdressel8336@eagle.fgcu.edu>
2019-05-16 19:10:40 -0400

    Added string empName and input. Also added Menu output.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int main() {
-    cout << "Huh?" << endl;
+    string empName;
+    cout << "Welcome to OraclProduction Ltd!\n";
+    cout << "It seems you are new! Please enter your name and we'll create an employee account for you.\n" ;
+    cin >> empName;
+    cout << "Welcome Aboard " + empName + "!\n";
+    cout << "1. Add Music Player\n" << "2. Add Movie Player\n" << "3. View Production Statistics\n"
+         << "4. Create New Employee\n" << "5. Exit";
     return 0;
 }
