883ed4eddc82a3fa434ea06d8b272b3ee21c9702
DresselAdam <amdressel8336@eagle.fgcu.edu>
2019-05-17 09:29:59 -0400

    test

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
     cout << "Welcome to OraclProduction Ltd!\n";
     cout << "It seems you are new! Please enter your name and we'll create an employee account for you.\n" ;
     cin >> empName;
-    cout << "Welcome Aboard " + empName + "!\n";
+    cout << "Welcome Aboard! " + empName + "!\n";
     cout << "1. Add Music Player\n" << "2. Add Movie Player\n" << "3. View Production Statistics\n"
          << "4. Create New Employee\n" << "5. Exit";
     return 0;
