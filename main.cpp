6082c4ac6823a7a5d45baec02439f06cfdd1cab1
Adam Dressel <dresseladam3@users.noreply.github.com>
2019-05-18 18:10:02 -0400

    Changed gitignore

--- main.cpp
+++ main.cpp
@@ -10,6 +10,5 @@ int main() {
     cout << "Welcome Aboard! " + empName + "!\n";
     cout << "1. Add Music Player\n" << "2. Add Movie Player\n" << "3. View Production Statistics\n"
          << "4. Create New Employee\n" << "5. Exit";
-    cout << "Test";
     return 0;
 }
